54f396d7c502915fac606437711495522c2f3380
Gavin Corlette <gavinccorlette@gmail.com>
2024-11-04 11:35:39 -0500

    added deliver_package and packages_to_deliver to Truck class

--- Truck.py
+++ Truck.py
@@ -8,3 +8,15 @@ class Truck:
 
     def __str__(self):
         return f'{self.package_list}, {self.address}, {self.departure_time}'
+
+    # Removes package from package_list when delivered
+    def deliver_package(self, package):
+        if package in self.package_list:
+            self.package_list.remove(package)
+
+    # Returns True if there are still packages in package_list
+    def packages_to_deliver(self):
+        if len(self.package_list) > 0:
+            return True
+        elif len(self.package_list) == 0:
+            return False
